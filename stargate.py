27ccb0a8edc8576d7bb83bd66501c96704890ff5
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2023-12-09 17:14:17 -0300

    stargate adicionado

---
+++ stargate.py
@@ -0,0 +1,133 @@
+import database, main, menu as m
+from prettytable import PrettyTable
+
+class Stargate:
+    def __init__(self, endereco, status_stargate, planeta):
+        self.endereco = endereco
+        self.status_stargate = status_stargate
+        self.planeta = planeta
+
+    def __repr__(self):
+        return f"Stargate(endereco='{self.endereco}', status_stargate='{self.status_stargate}', planeta={self.planeta})"
+
+def menu(conexao):
+    m.limpar_tela()
+    print(38 * "=")
+    print("Menu do Stargate")
+    print(38 * "=")
+    print("Escolha uma operação:")
+    print("1. Listar todos os Stargates")
+    print("2. Buscar Stargate")
+    print("3. Inserir Stargate")
+    print("4. Retornar")
+    print("5. Sair")
+    print(38 * "=")
+    print("Digite o número da sua escolha e pressione Enter:")
+    while True:
+        try:
+            escolha = int(input())
+            if 1 <= escolha <= 5:
+                processar_escolha(escolha, conexao)
+            else:
+                print("Por favor, insira um número entre 1 e 3.")
+        except ValueError:
+            print("Entrada inválida. Por favor, insira um número.")
+
+
+def processar_escolha(escolha, conexao):
+    if escolha == 1:
+        print("Você escolheu listar todos os stargates.")
+        query = "SELECT * FROM STARGATE;"
+        registros = database.consulta(conexao, query)
+        stargates = [Stargate(*registro) for registro in registros]
+        m.limpar_tela()
+        print("Listando todos os stargates")
+        imprimir_stargates(stargates)
+        menu(conexao)
+
+    elif escolha == 2:
+        m.limpar_tela()
+        print("Escolha o parâmetro de busca:")
+        print("1. Endereço")
+        print("2. Status do Stargate")
+        print("3. Planeta")
+        escolha = input("Digite o número do parâmetro: ")
+
+        parametro = ""
+        if escolha == "1":
+            parametro = "endereco"
+        elif escolha == "2":
+            parametro = "status_stargate"
+        elif escolha == "3":
+            parametro = "planeta"
+        else:
+            print("Escolha inválida")
+            return
+
+        valor = input(f"Digite o valor para {parametro}: ")
+        buscar(conexao, parametro, valor)
+        menu(conexao)
+    elif escolha == 3:
+        print("Você escolheu inserir Stargate.")
+    elif escolha == 4:
+        m.start(conexao)
+        
+        # Adicione sua lógica aqui
+    elif escolha == 5:
+        main.sair(conexao)
+        
+
+
+def imprimir_stargates(stargates):
+    #Usando o pretty table para a impressão
+    tabela = PrettyTable()
+    tabela.field_names = ["Endereço", "Status", "Planeta"]
+
+    for stargate in stargates:
+        tabela.add_row([stargate.endereco, stargate.status_stargate, stargate.planeta])
+    print(tabela)
+    print("Faça a análise dos dados, anote o que for necessário e aperte qualquer tecla para retornar ao menu")
+    input()
+
+def buscar(conexao, parametro, valor):
+    # Verifica se o valor é uma instância de string
+    if isinstance(valor, str):
+        # Converte para lowercase
+        valor = valor.lower()
+    # Usando parâmetros na consulta SQL
+    query = f"SELECT * FROM Stargate WHERE {parametro} = %s"
+    cursor = conexao.cursor()
+    cursor.execute(query, (valor,))
+    registros = cursor.fetchall()
+
+    stargates = [Stargate(*registro) for registro in registros]
+    m.limpar_tela()
+    print(f"Exibindo resultados da busca de Stargate com {parametro} igual a {valor}")
+    imprimir_stargates(stargates)
+
+    # Fecha o cursor após o uso (a conexão pode ser fechada em outro local)
+    cursor.close()
+
+def inserir(conexao, stargate):
+    try:
+        # Cria um cursor
+        cursor = conexao.cursor()
+        
+        # Prepara a instrução SQL
+        query = "INSERT INTO Stargate (endereco, status_stargate, planeta) VALUES (%s, %s, %s)"
+        
+        # Executa a instrução SQL
+        cursor.execute(query, (endereco, status_stargate, planeta))
+        
+        # Faz o commit das mudanças
+        conexao.commit()
+        
+        print("Stargate inserido com sucesso.")
+
+    except psycopg2.Error as e:
+        print("Erro ao inserir Stargate no banco de dados: ", e)
+        conexao.rollback()
+    finally:
+        # Fecha o cursor
+        cursor.close()
+        
