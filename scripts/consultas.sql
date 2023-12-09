907908c27c4d95d275aade16bf34556ec3498c98
Livia <liviafausto03@gmail.com>
2023-12-09 13:50:05 -0300

    fix: especificando esquema public nos scripts

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -1,6 +1,4 @@
--- Utilizar os comandos abaixo para mudar o nome do esquema
---show search_path;
---set search_path to stargate; -- Se o nome do esquema for "stargate"
+set search_path to public;
 
 select * from galaxia;
 
